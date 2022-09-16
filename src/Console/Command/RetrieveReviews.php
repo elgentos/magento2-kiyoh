bb0a6fa3ab3025b4b47a963dd88ee56aaddfe061
Jesse de Boer <jesse@elgentos.nl>
2022-09-16 14:27:12 +0200

    Implement per store Kiyoh reviews
    - Added a command and service to retrieve reviews
    - Changed the retrieval of reviews to get the reviews per storeview

---
+++ src/Console/Command/RetrieveReviews.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elgentos\Kiyoh\Console\Command;
+
+use Elgentos\Kiyoh\Service\RetrieveKiyohReviews;
+use Magento\Framework\Console\Cli;
+use Magento\Framework\Exception\LocalizedException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RetrieveReviews extends Command {
+
+    const COMMAND_NAME = 'elgentos:kiyoh:retrieve';
+    const COMMAND_DESCRIPTION = 'Retrieving Kiyoh reviews through the Publishing API';
+    private RetrieveKiyohReviews $service;
+
+    public function __construct(
+        RetrieveKiyohReviews $service,
+        string $name = null
+    )
+    {
+        parent::__construct($name ?? self::COMMAND_NAME);
+        $this->service = $service;
+    }
+
+    public function configure()
+    {
+        $this->setDescription(self::COMMAND_DESCRIPTION);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try {
+            $this->service->execute();
+        } catch (LocalizedException $e) {
+            $output->write($e->getMessage());
+            return Cli::RETURN_FAILURE;
+        }
+
+        return Cli::RETURN_SUCCESS;
+    }
+
+}
--- src/Cron/RetrieveReviews.php
+++ src/Cron/RetrieveReviews.php
@@ -9,108 +9,24 @@ declare(strict_types=1);
 
 namespace Elgentos\Kiyoh\Cron;
 
-use Exception;
-use Magento\Framework\HTTP\Client\Curl;
-use Magento\Variable\Model\ResourceModel\Variable as VariableResource;
-use Magento\Variable\Model\Variable;
-use Magento\Variable\Model\VariableFactory;
-use Psr\Log\LoggerInterface;
-use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Framework\Exception\CronException;
-use Elgentos\Kiyoh\Model\Config;
+use Elgentos\Kiyoh\Service\RetrieveKiyohReviews;
+use Magento\Framework\Exception\LocalizedException;
 
 class RetrieveReviews
 {
-    private const RATING_CODES = ['numberReviews', 'averageRating', 'recommendation'];
-
-    private Curl $curl;
-
-    private LoggerInterface $log;
-
-    private VariableFactory $variableFactory;
-
-    private Json $serializer;
-
-    private Config $config;
-
-    private VariableResource $variableResource;
+    private RetrieveKiyohReviews $service;
 
     public function __construct(
-        Curl $curl,
-        Json $serializer,
-        LoggerInterface $logger,
-        VariableFactory $variableFactory,
-        VariableResource $variableResource,
-        Config $config
-    ) {
-        $this->curl             = $curl;
-        $this->serializer       = $serializer;
-        $this->log              = $logger;
-        $this->variableFactory  = $variableFactory;
-        $this->config           = $config;
-        $this->variableResource = $variableResource;
+        RetrieveKiyohReviews $service
+    )
+    {
+        $this->service = $service;
     }
 
     /**
-     * @throws CronException
+     * @throws LocalizedException
      */
-    public function processAggregateScores(): void
-    {
-        if (!$this->config->getLocationId()) {
-            throw new CronException(__('Location ID missing, please set your location ID.'));
-        }
-
-        if (!$this->config->getApiKey()) {
-            throw new CronException(__('API key missing, please set your API key.'));
-        }
-
-        $this->curl->addHeader('X-Publication-Api-Token', $this->config->getApiKey());
-        $this->curl->get(sprintf(Config::PUBLICATION_URL, $this->config->getLocationId()));
-
-        $output = $this->serializer->unserialize($this->curl->getBody());
-
-        if (isset($output['errorCode'])) {
-            throw new CronException(
-                __(
-                    'Kiyoh API error: %1: %2',
-                    $output['errorCode'],
-                    $output['detailedError'][0]['message'] ?? null
-                )
-            );
-        }
-
-        try {
-            $this->saveToDb($output);
-        } catch (Exception $e) {
-            $this->log->critical($e->getMessage());
-        }
-    }
-
-    protected function saveToDb(array $data): void
-    {
-        foreach (self::RATING_CODES as $ratingCode) {
-            $reviewData = $data[$ratingCode] ?? null;
-            $variable   = $this->initVariable(sprintf('kiyoh_%s', $ratingCode));
-            $variable->setData('name', sprintf('Kiyoh %s', $ratingCode))
-                ->setData('html_value', $reviewData)
-                ->setData('plain_value', $reviewData);
-
-            try {
-                $this->variableResource->save($variable);
-            } catch (Exception $e) {
-                $this->log->critical($e->getMessage());
-            }
-        }
-    }
-
-    protected function initVariable(string $code, int $storeId = null): Variable
-    {
-        /** @var Variable $variable */
-        $variable = $this->variableFactory->create();
-
-        return $variable
-            ->setStoreId($storeId)
-            ->loadByCode($code)
-            ->setCode($code);
+    public function execute(): void {
+        $this->service->execute();
     }
 }
