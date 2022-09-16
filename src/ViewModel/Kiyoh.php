ed2a5c30e06c98c720d42a86aa4cd88238167187
Arjen Miedema <ArjenMiedema@users.noreply.github.com>
2022-09-16 11:59:21 +0200

    Refactor extension with coding standards (#5)
    The coding standards have been applied and the code is updated to match
    the standards.
    
    Co-authored-by: Arjen Miedema <arjen@elgentos.nl>

---
+++ src/ViewModel/Kiyoh.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright Elgentos. All rights reserved.
+ * https://www.elgentos.nl
+ */
+
+declare(strict_types=1);
+
+namespace Elgentos\Kiyoh\ViewModel;
+
+use Elgentos\Kiyoh\Model\Config;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Variable\Model\Variable;
+use Magento\Variable\Model\VariableFactory;
+
+class Kiyoh implements ArgumentInterface
+{
+    private StoreManagerInterface $storeManager;
+
+    private VariableFactory $variable;
+
+    private Config $config;
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        VariableFactory $variableFactory,
+        Config $config
+    ) {
+        $this->storeManager = $storeManager;
+        $this->variable     = $variableFactory;
+        $this->config       = $config;
+    }
+
+    public function getReviewCount(): int
+    {
+        return (int) $this->getVariableValueByCode('kiyoh_numberReviews');
+    }
+
+    public function getRating(): float
+    {
+        return (float) $this->getVariableValueByCode('kiyoh_averageRating');
+    }
+
+    public function getRatingPercentage(): float
+    {
+        return (float) $this->getVariableValueByCode('kiyoh_recommendation');
+    }
+
+    public function getKiyohCustomerUrl(): string
+    {
+        return $this->config->getKiyohUrl();
+    }
+
+    public function getVariableValueByCode(string $code): ?string
+    {
+        try {
+            $variable = $this->variable->create()
+                ->setStoreId($this->storeManager->getStore()->getId())
+                ->loadByCode($code);
+        } catch (NoSuchEntityException $e) {
+            return null;
+        }
+
+        return $variable instanceof Variable
+            ? $variable->getValue()
+            : null;
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->config->isEnabled();
+    }
+}
